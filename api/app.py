1ac133e41eeb58d54f467eac7c4add954972c121
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-26 18:38:28 +0300

    commit

--- api/app.py
+++ api/app.py
@@ -1,25 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+import requests
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
 url = "https://app.videas.fr/auth/signup/"
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+signup_url = "https://app.videas.fr/auth/signup/"
 
 @app.get("/create-account/{index}")
 def create_account(index: int):
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url)
-    email_input = driver.find_element(By.NAME, "email")
-    email_input.send_keys(f"mihai{index}.networkstudio@maildrop.cc")
-    password1_input = driver.find_element(By.NAME, "password1")
-    password1_input.send_keys("NetworkStudio123.")
-    password2_input = driver.find_element(By.NAME, "password2")
-    password2_input.send_keys("NetworkStudio123.")
-    signup_button = driver.find_element(By.CSS_SELECTOR, "#signup_form > button")
-    signup_button.click()
-    driver.quit()
-    return {"message": f"Account {index} created successfully."}
+    session = requests.Session()
+    response = session.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'}).get('value')
+    payload = {
+        'csrfmiddlewaretoken': csrf_token,
+        'email': f"mihai{index}.networkstudio@maildrop.cc",
+        'password1': "NetworkStudio123.",
+        'password2': "NetworkStudio123."
+    }
+    
+    headers = {
+        'Referer': url
+    }
+    
+    post_response = session.post(signup_url, data=payload, headers=headers)
+    if post_response.url == "https://app.videas.fr/auth/signup/":
+        return {"message": f"Account {index} already exists"}
+    return {"message": f"Account {index} created"}
