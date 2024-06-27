e3a6027b0e85a8ae10d16b40583325ecc660ec3d
mihai-ciorobitca <mihai.ciorobitca@outlook.com>
2024-06-27 10:19:46 +0300

    commit

--- register.py
+++ register.py
@@ -8,7 +8,7 @@ def create_account(index: int):
         response = get(url + str(i))
         print(i, response.text)
         with open("output/output" + str(index) + ".txt", "a") as f:
-            f.write(str(i)+ "\n")
+            f.write(response.text)
 
 threads = [Thread(target=create_account, args=(i,)) for i in range(1, 21)]
 [open("output/output" + str(i) + ".txt", "w").close() for i in range(1, 21)]
